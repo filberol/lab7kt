0c546416f91cd0fa35754362c69f21c09312f212
filberol <mkh.vadim@gmail.com>
2022-04-27 20:53:30 +0300

    HotFix

--- server/src/main/kotlin/lab6server/data/utilities/ConfigManager.kt
+++ server/src/main/kotlin/lab6server/data/utilities/ConfigManager.kt
@@ -30,9 +30,7 @@ class ConfigManager(private val language: LanguageManager) {
         config["CollectionPath"] = "./server/src/main/resources/"
         config["port"] = "5428"
         config["SoTimeout"] = "600000"
-        config["dbAddress"] = "jdbc:postgresql://localhost:5432/lab2prog"
-        config["dbUsername"] = "postgres"
-        config["dbPassword"] = "amd921"
+        config["dbAddress"] = "jdbc:postgresql://localhost:5555/studs"
     }
 
     fun getString(arg: String): String = config.getProperty(arg)
--- server/src/main/kotlin/lab6server/server/SqlHandler.kt
+++ server/src/main/kotlin/lab6server/server/SqlHandler.kt
@@ -6,6 +6,7 @@ import lab6server.data.utilities.LanguageManager
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
+import java.util.Scanner
 import kotlin.system.exitProcess
 
 /**
@@ -19,23 +20,34 @@ class SqlHandler(
     collection: CollectionManager
 ) {
     private val dbConnection: Connection by lazy { settleDatabaseConnection() }
+    private lateinit var dbPassword: String
+    private lateinit var dblogin: String
     val dataManager = SqlDataManager(dbConnection, collection)
     val userManager = SqlUserManager(dbConnection)
 
-
+    //Команда для прокида порта
+    //ssh -L <порт>:pg:5432 s<ISU>@se.ifmo.ru -p 2222
     private fun settleDatabaseConnection(): Connection {
         val databaseDriver = "org.postgresql.Driver"
         Class.forName(databaseDriver)
         try {
+            setVars()
             return DriverManager.getConnection(
                 config.getString("dbAddress"),
-                config.getString("dbUsername"),
-                config.getString("dbPassword")
+                dblogin,
+                dbPassword
             )
         } catch (e: SQLException) {
             println(language.getString("DBError"))
             exitProcess(1)
         }
 
+    }
+    private fun setVars() {
+        val scan = Scanner(System.`in`)
+        print("login")
+        dblogin = scan.nextLine()
+        print("password")
+        dbPassword = scan.nextLine()
     }
 }
